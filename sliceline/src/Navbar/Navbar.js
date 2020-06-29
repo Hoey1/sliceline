cadab1da45b23debb402a94064f5d5082602078b
Joey <joey@xm8.ninja>
2020-06-29 13:29:04 -0400

    Added Navbar with styled font-logo and text shadow

---
+++ sliceline/src/Navbar/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+import { pizzaRed } from "../Styles/colors";
+import { Title } from "../Styles/title";
+
+const NavbarStyled = styled.div`
+  background-color: ${pizzaRed};
+  padding: 10px;
+`;
+
+const Logo = styled(Title)`
+  font-size: 20px;
+  color: white;
+  text-shadow: 1px 1px 4px #380502;
+`;
+
+export function Navbar() {
+  return (
+    <NavbarStyled>
+      <Logo>Sliceline 🍕</Logo>
+    </NavbarStyled>
+  );
+}
