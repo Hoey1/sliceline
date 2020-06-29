951bc55c85e92378a9e2f31839e703c7bc2f4a3f
Joey <joey@xm8.ninja>
2020-06-29 13:06:38 -0400

    added styled components and Righteous - Open Sans from Google Fonts

--- sliceline/src/App.js
+++ sliceline/src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Open Sans', sans-serif;
+  }
+
+  h1, h2, h3 {
+    font-family: 'Righteous', cursive;
+  }
+`;
 
 function App() {
-  return <div>Hello Sliceline</div>;
+  return (
+    <>
+      <h1>Sliceline</h1>
+      <GlobalStyle />
+      <div>Hello Sliceline</div>
+    </>
+  );
 }
 
 export default App;
