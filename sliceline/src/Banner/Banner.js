034de3f471c478add2f1de1be51ad8ca86909f73
Joey <joey@xm8.ninja>
2020-06-29 15:16:58 -0400

    updated banner image, added contrast filter and applied z-index on navbar to bring it up front

--- sliceline/src/Banner/Banner.js
+++ sliceline/src/Banner/Banner.js
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 
 export const Banner = styled.div`
-  height: 500px;
+  height: 200px;
   background-image: url("img/banner.jpeg");
   background-position: center;
   background-size: cover;
+  filter: contrast(75%);
 `;
