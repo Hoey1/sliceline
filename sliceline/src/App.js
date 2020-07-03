7be06173427cf771760ca75548fa7093f84137b6
Joey <joey@xm8.ninja>
2020-07-03 11:03:21 -0400

    added new favicon, also added custom hook to update document title based on what user does/selects

--- sliceline/src/App.js
+++ sliceline/src/App.js
@@ -8,10 +8,12 @@ import { FoodDialog } from "./FoodDialog/FoodDialog";
 import { Order } from "./Order/Order";
 import { useOpenFood } from "./Hooks/useOpenFood";
 import { useOrders } from "./Hooks/useOrders";
+import { useTitle } from "./Hooks/useTitle";
 
 function App() {
   const openFood = useOpenFood();
   const orders = useOrders();
+  useTitle({ ...openFood, ...orders });
 
   return (
     <>
