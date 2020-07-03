1091b983bbe26f65c04edb8306ef23afd53ffdb7
Joey <joey@xm8.ninja>
2020-07-02 21:28:15 -0400

    Created two new hooks useOpenFood and useOrders - spreading those props throughout app

--- sliceline/src/App.js
+++ sliceline/src/App.js
@@ -6,18 +6,21 @@ import { Menu } from "./Menu/Menu";
 import { GlobalStyle } from "./Styles/GlobalStyle";
 import { FoodDialog } from "./FoodDialog/FoodDialog";
 import { Order } from "./Order/Order";
+import { useOpenFood } from "./Hooks/useOpenFood";
+import { useOrders } from "./Hooks/useOrders";
 
 function App() {
-  const [openFood, setOpenFood] = useState();
+  const openFood = useOpenFood();
+  const orders = useOrders();
 
   return (
     <>
       <GlobalStyle />
-      <FoodDialog openFood={openFood} setOpenFood={setOpenFood} />
+      <FoodDialog {...openFood} {...orders} />
       <Navbar />
-      <Order />
+      <Order {...orders} />
       <Banner />
-      <Menu setOpenFood={setOpenFood} />
+      <Menu {...openFood} />
     </>
   );
 }
